5045c5b7d502aa043fc37ac4d3f90ba4e19f889e
RoaldHaahr <roald_832@hotmail.com>
2015-12-11 12:49:58 +0100

    11/12/2015

---
+++ myProfile.php
@@ -0,0 +1,48 @@
+<?php include_once('header.php'); ?>
+<?php include_once('nav.php'); ?>
+
+	<section class="page" id="loggedin-user-profile-page">
+		
+			<div class="container-fluid">
+				
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="page-title">Michael White</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<figure class="profile-picture">
+							<img src="images/michaelwhite.png">
+						</figure>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<h4 class="user-role">-Tourist-</h4>
+						<p id="user-description">
+							Graphic designer and art lover. I travel to meet new inspiring people and to admire architecture and art. I love to write short stories about places I visited.
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12" id="user-information">
+						<p>
+							<strong>E-mail: </strong>m.white@gmail.com
+						</p>
+						<p>
+							<strong>Nationality: </strong>French
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<a class="link-button red-link" id="edit-link" href="edit.php">EDIT</a>
+					</div>
+				</div>
+
+			</div>
+
+	</section>
+
+<?php include_once('footer.php'); ?>
---
+++ userProfile.php
@@ -0,0 +1,50 @@
+<?php include_once('header.php'); ?>
+<?php include_once('nav.php'); ?>
+
+	<section class="page" id="user-profile-page">
+		
+			<div class="container-fluid">
+				
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="page-title">Michael White</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-4">
+						<figure class="profile-picture">
+							<img src="images/michaelwhite.png">
+						</figure>
+						<h4 class="user-role">-Tourist-</h4>
+						<p id="user-description">
+							Graphic designer and art lover. I travel to meet new inspiring people and to admire architecture and art. I love to write short stories about places I visited.
+						</p>
+						<p id="nationality">
+							<strong>Nationality: </strong>French
+						</p>						
+					</div>
+
+					<div class="col-md-4">
+						<form id="contact-form" action="handleMail.php" method="post">
+							<h3>Get in contact with Michael</h3>
+							<div><input type="text" name="subject" placeholder="Subject"></div>
+							<div><textarea name="message" placeholder="Your message"></textarea></div>
+							<input type="submit" class="link-button red-link" value="Contact">
+						</form>
+						
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						
+					</div>
+				</div>
+				</div>
+
+			</div>
+
+	</section>
+
+<?php include_once('footer.php'); ?>
