ac75dc516e49ebccfa06cdbf9b410af7d027cc89
m-thrane <thrane.pedrsen@gmail.com>
2015-12-17 18:50:02 +0100

    log in

--- addUser.php
+++ addUser.php
@@ -2,25 +2,23 @@
 
 	include_once('connection.php');
 
+	$id = NULL;
+
 	$sql = "INSERT INTO users (email, password)
 			VALUES (:email, :password);";
-
-$password=md5($_POST['password']);
-
 	try {
 		$st = $conn->prepare($sql);
-<<<<<<< HEAD
 		$st->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
 		$st->bindValue(':password', md5($_POST['password']), PDO::PARAM_STR);
-=======
-		$st->bindValue(':name', $_POST['username'], PDO::PARAM_STR);
-		$st->bindValue(':password', $password, PDO::PARAM_STR);
->>>>>>> origin/master
 		$st->execute();
+		$id = $conn->lastInsertId();
 	} catch (PDOException $e) {
 		echo "Server error - try again! " . $e->getMessage();
 	}
 
+	session_start();
+
+	$_SESSION['userID'] = $id;
 	$conn = null;
 	
 	header('Location:' . 'signUpCompletion.php');
