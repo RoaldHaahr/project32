f41fab70040e00490ae3f70be1538b5c240bef97
RoaldHaahr <roald_832@hotmail.com>
2015-12-17 20:38:04 +0100

    in

--- js/app.js
+++ js/app.js
@@ -1,25 +1,17 @@
 $('a#nearest-events').click(function(e){
 	e.preventDefault();
-
 	var startPos;
-
 	function showPosition(position) {
 		var startPos = position;
-
 		var distanceCoords = {
 			'x': position.coords.latitude,
 			'y': position.coords.longitude
 		};
-
 		$.post('getNearestEvents.php', { coordinates: JSON.stringify(distanceCoords) }, processData);
-
 		function processData(data) {
 			$('div#events-list').html(data);
 		}
 	}
-
-	navigator.geolocation.getCurrentPosition(showPosition);
-
-	
+	navigator.geolocation.getCurrentPosition(showPosition);	
 });
 
